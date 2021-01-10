a2302743877af3f63feef01cb804269230da88e3
youshy <devilyoushy@gmail.com>
2021-01-10 12:03:09 +0100

    Challenge_02 solved

--- SET_01/challenge.go
+++ SET_01/challenge.go
@@ -3,31 +3,67 @@ package challenge
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 )
 
 // Challenge 01
-func Solve(input []byte) ([]byte, error) {
+func Solve01(input []byte) ([]byte, error) {
 	dec, err := decodeHex(input)
 	if err != nil {
 		fmt.Printf("failed to decode hex: %s", err)
 		return nil, err
 	}
 
-	f := base64Encode(dec)
+	res := base64Encode(dec)
 
-	fmt.Printf("%s\n", f)
+	fmt.Printf("%s\n", res)
 
-	return f, nil
+	return res, nil
 }
 
-func decodeHex(input []byte) ([]byte, error) {
-	dst := make([]byte, hex.DecodedLen(len(input)))
-	_, err := hex.Decode(dst, input)
+// Challenge 02
+func Solve02(input1, input2 []byte) ([]byte, error) {
+	dec1, err := decodeHex(input1)
+	if err != nil {
+		fmt.Printf("failed to decode hex: %s", err)
+		return nil, err
+	}
+
+	dec2, err := decodeHex(input2)
+	if err != nil {
+		fmt.Printf("failed to decode hex: %s", err)
+		return nil, err
+	}
+
+	res, err := fixedXorDecrypt(dec1, dec2)
 	if err != nil {
 		return nil, err
 	}
-	return dst, nil
+
+	fmt.Printf("%s\n", res)
+
+	return res, nil
+}
+
+//
+// Shared functions
+//
+
+func decodeHex(input []byte) ([]byte, error) {
+	dec := make([]byte, hex.DecodedLen(len(input)))
+	_, err := hex.Decode(dec, input)
+	if err != nil {
+		return nil, err
+	}
+	return dec, nil
+}
+
+func encodeHex(input []byte) []byte {
+	eb := make([]byte, hex.EncodedLen(len(input)))
+	hex.Encode(eb, input)
+
+	return eb
 }
 
 func base64Encode(input []byte) []byte {
@@ -35,4 +71,17 @@ func base64Encode(input []byte) []byte {
 	base64.StdEncoding.Encode(eb, input)
 
 	return eb
+}
+
+func fixedXorDecrypt(input1, input2 []byte) ([]byte, error) {
+	if len(input1) != len(input2) {
+		return nil, errors.New("inputs have mismatched lenghts")
+	}
+	dec := make([]byte, len(input1))
+	for i := 0; i < len(input1); i++ {
+		dec[i] = input1[i] ^ input2[i]
+	}
+
+	fmt.Printf("FixedXORDecrypt %s\n", dec)
+	return dec, nil
 }
--- SET_01/challenge_test.go
+++ SET_01/challenge_test.go
@@ -10,7 +10,18 @@ func TestChallenge01(t *testing.T) {
 	input := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	expected := []byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
 
-	res, err := Solve(input)
+	res, err := Solve01(input)
+
+	assert.Equal(t, res, expected, "they should be equal")
+	assert.NoError(t, err, "there should be no error")
+}
+
+func TestChallenge02(t *testing.T) {
+	input1 := []byte("1c0111001f010100061a024b53535009181c")
+	input2 := []byte("686974207468652062756c6c277320657965")
+	expected, _ := decodeHex([]byte("746865206b696420646f6e277420706c6179"))
+
+	res, err := Solve02(input1, input2)
 
 	assert.Equal(t, res, expected, "they should be equal")
 	assert.NoError(t, err, "there should be no error")
